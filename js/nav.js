32a64b0d93e7d82567f07372426e703494226c89
Miranda Tchelidze <mitchelidze@gmail.com>
2020-09-22 20:12:24 +0400

    updating

--- js/nav.js
+++ js/nav.js
@@ -4,9 +4,6 @@ const nav = document.querySelector('.nav');
 const menuNav = document.querySelector('.menu-nav');
 const navItems = document.querySelectorAll('.menu-nav__item');
 
-// const menuSocs = document.querySelectorAll('.menu-soc');
-// const menuIcons = document.querySelectorAll('.menu-soc__icons');
-
 let showMenu = false;
 
 menuBtn.addEventListener('click', toggleMenu);
@@ -18,18 +15,12 @@ function toggleMenu() {
     menuNav.classList.add('open');
     navItems.forEach(item => item.classList.add('open'));
 
-    // menuSocs.forEach(soc => soc.classList.add('open'));
-    // menuIcons.forEach(icon => icon.classList.add('open'));
-
     showMenu = true;
   } else {
     hamburger.classList.remove('open');
     nav.classList.remove('open');
     menuNav.classList.remove('open');
     navItems.forEach(item => item.classList.remove('open'));
-
-    // menuSocs.forEach(soc => soc.classList.remove('open'));
-    // menuIcons.forEach(icon => icon.classList.remove('open'));
 
     showMenu = false;
   }
